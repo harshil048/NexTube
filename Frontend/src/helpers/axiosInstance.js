0263c848785cd5492b8027cacd66a3a3baa2bcce
harshil048 <harshilbambhroliya048@gmail.com>
2024-06-04 20:36:52 +0530

    Done

---
+++ Frontend/src/helpers/axiosInstance.js
@@ -0,0 +1,10 @@
+import axios from "axios";
+import { BASE_URL } from "../../constant";
+
+
+const axiosInstance = axios.create();
+
+axiosInstance.defaults.baseURL = BASE_URL;
+axiosInstance.defaults.withCredentials = true;
+
+export default axiosInstance;
